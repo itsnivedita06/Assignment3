2414165da3094b6298d2533762ebc0e22c96048f
itsnivedita06 <itsnivedita06@gmail.com>
2023-06-25 09:15:02 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState({});
+  const [successMessage, setSuccessMessage] = useState('');
+
+  const validateForm = () => {
+    let formIsValid = true;
+    const newErrors = {};
+
+    if (!name) {
+      formIsValid = false;
+      newErrors['name'] = 'Please enter your name';
+    }
+
+    if (!email) {
+      formIsValid = false;
+      newErrors['email'] = 'Please enter your email address';
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      formIsValid = false;
+      newErrors['email'] = 'Please enter a valid email address';
+    }
+
+    if (!password) {
+      formIsValid = false;
+      newErrors['password'] = 'Please enter your password';
+    }
+
+    setErrors(newErrors);
+    return formIsValid;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (validateForm()) {
+      setSuccessMessage('Registration successful!');
+      setName('');
+      setEmail('');
+      setPassword('');
+    }
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+      {successMessage && <div className="success">{successMessage}</div>}
+      <form className='container' onSubmit={handleSubmit}>
+          <h2>Login Here</h2>
+          
+
+          <input type="text" className="input" placeholder="Name" value={name}
+            onChange={(e) => setName(e.target.value)} />
+          {errors.name && <span className="error">{errors.name}</span>}
+        
+          <input type="email" className="input" placeholder="Email" value={email}
+            onChange={(e) => setEmail(e.target.value)} />
+          {errors.email && <span className="error">{errors.email}</span>}
+
+          <input type="password" className="input" placeholder="Password" value={password}
+            onChange={(e) => setPassword(e.target.value)} />
+          {errors.password && <span className="error">{errors.password}</span>}
+        
+          <button id="submit" type="submit">Log In</button>
+      </form>
     </div>
   );
 }
